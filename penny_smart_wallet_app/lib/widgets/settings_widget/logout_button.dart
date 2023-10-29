95c7f3eb7482374032385f8d69d6387899b80cf3
batuhans <afrousse1998@gmail.com>
2023-10-30 01:43:22 +0300

    add ✨: adding widgets

---
+++ penny_smart_wallet_app/lib/widgets/settings_widget/logout_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:penny_smart_wallet/Screens/view_login/login_view.dart';
+
+class ExitIconWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => LoginPageView(),
+          ),
+        );
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: CircleAvatar(
+              backgroundColor: Colors.red.shade100,
+              radius: 25,
+              child: Icon(
+                Ionicons.exit,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          Text(
+            "Logout",
+            style: TextStyle(color: Colors.red, fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ penny_smart_wallet_app/lib/widgets/settings_widget/setting_item.dart
@@ -0,0 +1,66 @@
+import 'package:penny_smart_wallet/widgets/settings_widget/forward_button.dart';
+import 'package:flutter/material.dart';
+
+class SettingItem extends StatelessWidget {
+  final String title;
+  final Color bgColor;
+  final Color iconColor;
+  final IconData icon;
+  final Function() onTap;
+  final String? value;
+  const SettingItem({
+    super.key,
+    required this.title,
+    required this.bgColor,
+    required this.iconColor,
+    required this.icon,
+    required this.onTap,
+    this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: bgColor,
+            ),
+            child: Icon(
+              icon,
+              color: iconColor,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+          const Spacer(),
+          value != null
+              ? Text(
+                  value!,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Color.fromARGB(255, 158, 158, 158),
+                  ),
+                )
+              : const SizedBox(),
+          const SizedBox(width: 20),
+          ForwardButton(
+            onTap: onTap,
+          ),
+        ],
+      ),
+    );
+  }
+}
