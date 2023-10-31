df3d93adad5c1580a7589211e0e1319852dd2946
batuhans <afrousse1998@gmail.com>
2023-11-01 02:54:37 +0300

    update 🆕 : update main file directory

--- penny_smart_wallet_app/lib/main.dart
+++ penny_smart_wallet_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:penny_smart_wallet/Screens/splashscreen.dart';
+import 'package:penny_smart_wallet/Views/view_splashscreen/splashscreen.dart';
 
 import 'data/model/add_date.dart';
 
