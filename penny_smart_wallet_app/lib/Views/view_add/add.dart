43444c942049553bf46a81ede4b9d27c53e91636
batuhans <afrousse1998@gmail.com>
2023-11-01 02:55:48 +0300

    update 🆕 : add directory

---
+++ penny_smart_wallet_app/lib/Views/view_add/add.dart
@@ -0,0 +1,322 @@
+import 'package:flutter/material.dart';
+import 'package:penny_smart_wallet/data/model/add_date.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class Add_Screen extends StatefulWidget {
+  const Add_Screen({super.key});
+
+  @override
+  State<Add_Screen> createState() => _Add_ScreenState();
+}
+
+class _Add_ScreenState extends State<Add_Screen> {
+  final box = Hive.box<Add_data>('data');
+  DateTime date = new DateTime.now();
+  String? selctedItem;
+  String? selctedItemi;
+  final TextEditingController expalin_C = TextEditingController();
+  FocusNode ex = FocusNode();
+  final TextEditingController amount_c = TextEditingController();
+  FocusNode amount_ = FocusNode();
+  final List<String> _item = [
+    'Food',
+    "Transfer",
+    "Transportation",
+    "Education",
+    "Drinks",
+    "Bills",
+    "Work"
+  ];
+  final List<String> _itemei = [
+    'Income',
+    "Spending",
+  ];
+  @override
+  void initState() {
+    super.initState();
+    ex.addListener(() {
+      setState(() {});
+    });
+    amount_.addListener(() {
+      setState(() {});
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            _backButton(context),
+            Positioned(
+              top: 120,
+              child: main_container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container main_container() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: const Color.fromARGB(255, 34, 34, 34),
+      ),
+      height: 550,
+      width: 340,
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          name(),
+          SizedBox(height: 30),
+          amount(),
+          SizedBox(height: 30),
+          How(),
+          SizedBox(height: 30),
+          date_time(),
+          Spacer(),
+          save(),
+          SizedBox(height: 25),
+        ],
+      ),
+    );
+  }
+
+  GestureDetector save() {
+    return GestureDetector(
+      onTap: () {
+        var add = Add_data(
+            selctedItemi!, amount_c.text, date, expalin_C.text, selctedItem!);
+        box.add(add);
+        Navigator.of(context).pop();
+      },
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.deepPurple,
+        ),
+        width: 120,
+        height: 50,
+        child: Text(
+          'Save',
+          style: TextStyle(
+            fontFamily: 'f',
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+            fontSize: 17,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget date_time() {
+    return Container(
+      alignment: Alignment.bottomLeft,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 2, color: Color(0xffC5C5C5))),
+      width: 300,
+      child: TextButton(
+        onPressed: () async {
+          DateTime? newDate = await showDatePicker(
+              context: context,
+              initialDate: date,
+              firstDate: DateTime(2023),
+              lastDate: DateTime(2025));
+          if (newDate == Null) return;
+          setState(() {
+            date = newDate!;
+          });
+        },
+        child: Text(
+          'Date : ${date.year} / ${date.day} / ${date.month}',
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding How() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 2,
+            color: Color(0xffC5C5C5),
+          ),
+        ),
+        child: DropdownButton<String>(
+          value: selctedItemi,
+          onChanged: ((value) {
+            setState(() {
+              selctedItemi = value!;
+            });
+          }),
+          items: _itemei
+              .map((e) => DropdownMenuItem(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Row(
+                        children: [
+                          Text(
+                            e,
+                            style: TextStyle(fontSize: 18),
+                          )
+                        ],
+                      ),
+                    ),
+                    value: e,
+                  ))
+              .toList(),
+          selectedItemBuilder: (BuildContext context) => _itemei
+              .map((e) => Row(
+                    children: [Text(e)],
+                  ))
+              .toList(),
+          hint: Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: Text(
+              'How',
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          dropdownColor: Colors.white,
+          isExpanded: true,
+          underline: Container(),
+        ),
+      ),
+    );
+  }
+
+  Padding amount() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        focusNode: amount_,
+        controller: amount_c,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          labelText: 'amount',
+          labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(width: 2, color: Color(0xffC5C5C5))),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(width: 2, color: Color(0xff368983))),
+        ),
+      ),
+    );
+  }
+
+  Padding name() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        width: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 2,
+            color: Color(0xffC5C5C5),
+          ),
+        ),
+        child: DropdownButton<String>(
+          value: selctedItem,
+          onChanged: ((value) {
+            setState(() {
+              selctedItem = value!;
+            });
+          }),
+          items: _item
+              .map((e) => DropdownMenuItem(
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 40,
+                            child: Image.asset('images/${e}.png'),
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            e,
+                            style: TextStyle(fontSize: 18),
+                          )
+                        ],
+                      ),
+                    ),
+                    value: e,
+                  ))
+              .toList(),
+          selectedItemBuilder: (BuildContext context) => _item
+              .map((e) => Row(
+                    children: [
+                      Container(
+                        width: 42,
+                        child: Image.asset('images/${e}.png'),
+                      ),
+                      SizedBox(width: 5),
+                      Text(e)
+                    ],
+                  ))
+              .toList(),
+          hint: Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: Text(
+              'Name',
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          dropdownColor: Colors.white,
+          isExpanded: true,
+          underline: Container(),
+        ),
+      ),
+    );
+  }
+
+  Widget _backButton(BuildContext context) {
+    return Positioned(
+      top: 10,
+      left: 0,
+      child: InkWell(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(left: 0, top: 10, bottom: 10),
+                child:
+                    Icon(Icons.keyboard_arrow_left, color: Colors.grey.shade50),
+              ),
+              Text('Back',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey.shade50))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
